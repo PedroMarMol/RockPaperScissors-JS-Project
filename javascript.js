487c78fa4a3bb478663fc5f2751e8bde4fed443c
Martos <ocularenginemartos@gmail.com>
2022-03-16 19:16:04 +0100

    Add 5 rounds game function. It can be coded way simpler.

--- javascript.js
+++ javascript.js
@@ -1,40 +1,60 @@
 let gameChoice = ["Paper", "Rock","Scissors"];
-var playerSelection = prompt("What do you choose? Rock, Paper or Scissors?");
+var playerSelection;
+const computerSelection = computerPlay();
+let playerCounter = 0;
+let computerCounter = 0;
+let winner;
 var scissors = "Scissors";
 var paper = "Paper";
 var rock = "Rock";
 
-function computerPlay() {
-    return gameChoice[Math.floor(Math.random() * gameChoice.length)];
+function computerPlay() {                                                       // Chooses a random number and convert it so it 
+    return gameChoice[Math.floor(Math.random() * gameChoice.length)];           // ends up choosing a word of the three (rock, paper or scissors).
 }
 
-function gameRound(playerSelection, computerSelection) {
-    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+function gameRound(playerSelection, computerSelection) {                        // function that plays a single round of the game, 
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {    // checking every case one by one and returning the winner.
         return "You have tied";
     }   else if (playerSelection.toUpperCase() === scissors.toUpperCase() && computerSelection.toUpperCase() === paper.toUpperCase()) {
-        return "You win! Scissors beats Paper";
+        return "You win!";
         }   else if (playerSelection.toUpperCase() === paper.toUpperCase() && computerSelection.toUpperCase() === scissors.toUpperCase()) {
-            return "You lose! Scissors beats Paper";
+            return "You lose!";
             }   else if (playerSelection.toUpperCase() === scissors.toUpperCase() && computerSelection.toUpperCase() === rock.toUpperCase()) {
-                return "You lose! Rock beats Scissors";
+                return "You lose!";
                 }   else if (playerSelection.toUpperCase() === rock.toUpperCase() && computerSelection.toUpperCase() === scissors.toUpperCase()) {
-                    return "You win! Rock beats Scissors";
+                    return "You win!";
                     }   else if (playerSelection.toUpperCase() === rock.toUpperCase() && computerSelection.toUpperCase() === paper.toUpperCase()) {
-                         return "You lose! Paper beats Rock";
+                         return "You lose!";
                         }   else if (playerSelection.toUpperCase() === paper.toUpperCase() && computerSelection.toUpperCase() === rock.toUpperCase()) {
-                            return "You win! Paper beats Rock";
+                            return "You win!";
                             } else {
                                 return "You didn't type a correct answer (Rock, Paper or Scissors).";
                             }
 }
 
-function game() {
+function game() {                       // creates a 5 round game and adds on the win-loses of the player and the computer
     for (let i = 0; i < 5; i++) {
-        gameRound 
-    }
+        playerSelection = prompt("What is your choice? Rock, Paper or Scissors?");
+        console.log(gameRound(playerSelection, computerSelection));
+        if (gameRound(playerSelection, computerSelection) === "You win!"){
+            playerCounter++;
+        } else if (gameRound(playerSelection, computerSelection) === "You lose!") {
+            computerCounter++;
+        } else if (gameRound(playerSelection, computerSelection) === "You have tied") {
+            computerCounter++;
+            playerCounter++;
+        }
+
+        if (computerCounter > playerCounter) {
+            winner = "The Computer";
+        } else {winner = "You"}
+    }   
+
+    console.log("Your points: " + playerCounter + " Computer Points: " + computerCounter);
+    console.log(winner +" won!");
 }
 
-const computerSelection = computerPlay();
-console.log(gameRound(playerSelection, computerSelection));
+game();
+
 
 

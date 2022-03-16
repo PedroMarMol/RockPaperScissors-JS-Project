79f2c9ffc0d3cdfcb61f527dd71fd3f23d613275
Martos <ocularenginemartos@gmail.com>
2022-03-16 13:25:44 +0100

    change return instead of console.log on every condition and bugfix

--- javascript.js
+++ javascript.js
@@ -1,35 +1,40 @@
 let gameChoice = ["Paper", "Rock","Scissors"];
-var computerSelection;
 var playerSelection = prompt("What do you choose? Rock, Paper or Scissors?");
 var scissors = "Scissors";
 var paper = "Paper";
 var rock = "Rock";
 
 function computerPlay() {
-    computerSelection = gameChoice[Math.floor(Math.random() * gameChoice.length)];
+    return gameChoice[Math.floor(Math.random() * gameChoice.length)];
 }
 
 function gameRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-        console.log("You have tied");
+        return "You have tied";
     }   else if (playerSelection.toUpperCase() === scissors.toUpperCase() && computerSelection.toUpperCase() === paper.toUpperCase()) {
-        console.log("You win! Scissors beats Paper");
+        return "You win! Scissors beats Paper";
         }   else if (playerSelection.toUpperCase() === paper.toUpperCase() && computerSelection.toUpperCase() === scissors.toUpperCase()) {
-            console.log("You lose! Scissors beats Paper");
+            return "You lose! Scissors beats Paper";
             }   else if (playerSelection.toUpperCase() === scissors.toUpperCase() && computerSelection.toUpperCase() === rock.toUpperCase()) {
-                console.log("You lose! Rock beats Scissors");
+                return "You lose! Rock beats Scissors";
                 }   else if (playerSelection.toUpperCase() === rock.toUpperCase() && computerSelection.toUpperCase() === scissors.toUpperCase()) {
-                    console.log("You win! Rock beats Scissors");
+                    return "You win! Rock beats Scissors";
                     }   else if (playerSelection.toUpperCase() === rock.toUpperCase() && computerSelection.toUpperCase() === paper.toUpperCase()) {
-                        console.log("You lose! Paper beats Rock");
+                         return "You lose! Paper beats Rock";
                         }   else if (playerSelection.toUpperCase() === paper.toUpperCase() && computerSelection.toUpperCase() === rock.toUpperCase()) {
-                            console.log("You win! Paper beats Rock");
+                            return "You win! Paper beats Rock";
                             } else {
-                                console.log("You didn't type a correct answer (Rock, Paper or Scissors).")
+                                return "You didn't type a correct answer (Rock, Paper or Scissors).";
                             }
 }
 
-computerPlay();
-gameRound(playerSelection, computerSelection);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        gameRound 
+    }
+}
+
+const computerSelection = computerPlay();
+console.log(gameRound(playerSelection, computerSelection));
 
 
